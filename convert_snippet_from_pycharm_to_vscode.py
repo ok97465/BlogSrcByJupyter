4cafabdecf7dae8a6b9a7c801cab044e8f9cba39
ok97465 <ok97465@gmail.com>
2018-11-13 23:34:56 +0900

    conver_pycharm_snippet_to_vscode

--- convert_snippet_from_pycharm_to_vscode.py
+++ convert_snippet_from_pycharm_to_vscode.py
@@ -5,12 +5,12 @@
 """
 
 import re
-from typing import NamedTuple, TextIO
+from typing import NamedTuple, TextIO, List
 
 
 class SnippetInfo(NamedTuple):
     description: str
-    code: str
+    code_list: List[str]
 
 
 def extract_abbreviation(abbreviation_str):
@@ -31,6 +31,40 @@ def extract_abbreviation(abbreviation_str):
         abbreviation_str = abbreviation_str[1:]
     
     return abbreviation_str.strip()
+
+def split_code_str_of_ipynb(code_str) -> List[str]:
+    """ipybn의 code 문자열을 명령별로 분할하여 List에 저장
+    
+    Parameters
+    ----------
+    code_str : str
+        ipynb의 code 문자열
+    
+    Returns
+    -------
+    List[str]
+        List[code]
+    """
+
+    code_str = code_str.replace('\\n', '')
+
+    cursor_order = re.findall(r'\$([0-9])\$', code_str)
+
+    if cursor_order:
+        cursor_order.sort()
+        n_cursor_order = len(cursor_order)
+        for idx, order in enumerate(cursor_order):
+            if idx == (n_cursor_order-1) and '$END$' not in code_str:
+                code_str = code_str.replace(f'${order}$', f'$0')
+            else:
+                code_str = code_str.replace(f'${order}$', f'${order}')
+
+    code_str = code_str.replace('$END$', '$0')
+
+    code_list = code_str.split(',\n')
+    code_list = [code.strip() for code in code_list if code.strip() != '']
+    return code_list
+
 
 
 def write_python_json_header(fp):
@@ -55,7 +89,7 @@ def write_python_json_header(fp):
     fp.write('\t// 	\t"$2"\n')
     fp.write('\t// 	],\n')
     fp.write('\t// 	"description": "Log output to console"\n')
-    fp.write('\t// }\n')
+    fp.write('\t// }\n\n')
 
 
 if __name__ == "__main__":
@@ -80,16 +114,28 @@ if __name__ == "__main__":
             abbreviation = extract_abbreviation(snippet_str[1])
             snippets[abbreviation] = SnippetInfo(
                 description=snippet_str[0],
-                code=snippet_str[3])
+                code_list=split_code_str_of_ipynb(snippet_str[3]))
         
         print(f'Snippets = {list(snippets.keys())}')
 
     with open('python.json', 'w') as fp:
         write_python_json_header(fp)
-        for abbreviation in list(snippets.keys()):
+        n_key = len(snippets.keys())
+        for idx, abbreviation in enumerate(list(snippets.keys())):
             fp.write(f'\t"{snippets[abbreviation].description}": {{\n')
             fp.write(f'\t "prefix": "{abbreviation}",\n')
             fp.write(f'\t "body": [\n')
+
+            n_code = len(snippets[abbreviation].code_list)
+            for idx_code, code  in enumerate(snippets[abbreviation].code_list):
+                if idx_code < n_code-1:
+                    fp.write(f'\t\t{code},\n')
+                else:
+                    fp.write(f'\t\t{code}\n')
+
             fp.write(f'\t ]\n')
-            fp.write(f'\t}}\n')
+            if idx < n_key - 1:
+                fp.write(f'\t}},\n')
+            else:
+                fp.write(f'\t}}\n')
         fp.write('}')
