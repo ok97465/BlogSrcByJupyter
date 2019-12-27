82e8289830d477a0cca278df9fec38f35b634975
ok97465 <ok97465@gmail.com>
2019-12-27 17:14:21 +0900

    Equidistant of ellipse

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export MPLCURSORS='{"multiple": 1}'
-export PYTHONPATH=/home/ok97465/codepy/Radar
-~/anaconda3/bin/python /home/ok97465/private/BlogSrcByJupyter/run_jupyter.py
+export PYTHONPATH=/home/ok97465/python/scientific
+~/anaconda3/bin/python /home/ok97465/python/BlogSrcByJupyter/run_jupyter.py
