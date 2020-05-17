5d48b53aec52ddec4646c6adfcc46431df27a951
ok97465 <ok97465@gmail.com>
2020-05-17 19:23:11 +0900

    update vim

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/zsh
 export MPLCURSORS='{"multiple": 1}'
 export PYTHONPATH=/home/ok97465/codepy/scientific
+source ~/anaconda3/bin/activate 
 ~/anaconda3/bin/python /home/ok97465/codepy/BlogSrcByJupyter/run_jupyter.py
